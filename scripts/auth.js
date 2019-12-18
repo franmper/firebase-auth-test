3710466b37d42bf116074a8ee6c29049892fdf38
franmper <38388588+franmper@users.noreply.github.com>
2019-12-18 16:52:50 -0300

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,42 +1,48 @@
-let q = (x) => document.querySelector(x);
+let q = x => document.querySelector(x);
+
+
+// status
+auth.onAuthStateChanged(user => {   
+    if (user) {
+        db.collection("guides").get().then(snapshot => {
+            setupGuides(snapshot.docs);
+        })
+    } else {
+        setupGuides([])
+    }
+});
 
  // singup
-
  const signupForm = q('#signup-form');
  signupForm.addEventListener('submit', (e) => {
      e.preventDefault();
      const email = signupForm['signup-email'].value;
      const password = signupForm['signup-password'].value;
-
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         const modal = q('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
     });
  });
-
+ 
 // logout
-
 const logout = q('#logout');
 logout.addEventListener('click', e => {
     e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('The User has Signed Out');
-    });
+    auth.signOut();
 });
 
 // login
-
 const loginForm = q('#login-form');
 loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
-
     auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         const modal = q('#modal-login');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
         });
 });
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,29 @@
+// data
+const guideList = q(".guides");
+
+const setupGuides = data => {
+  if (data.length) {
+    let html = "";
+    data.forEach(doc => {
+      const guide = doc.data();
+      html += `
+        <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+        </li>
+      `;
+    });
+
+    guideList.innerHTML = html
+  } else {
+    guideList.innerHTML = "<h5 class='center-align'>Login to View the Guides</h5>"
+  }
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
-
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
-
   var items = document.querySelectorAll('.collapsible');
   M.Collapsible.init(items);
-
 });
